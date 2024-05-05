a8a5c85411546e6190819d8df70a479f3fbeab7e
bblks <168915058+bblks@users.noreply.github.com>
2024-05-05 19:19:53 +0300

    Update bot.py

--- ZeMusic/plugins/play/bot.py
+++ ZeMusic/plugins/play/bot.py
@@ -25,7 +25,7 @@ async def BotMusic(client: Client, message: Message):
             rank = italy
     except Exception as e:
         print(e)
-    await message.reply_text(f"<b>⌯ مرحباً عزيزي :</b> {rank}\n<b>⌯ ما هي الاغنيه التي تريد تشغيلها أو البحث عنها</b>")
+    await message.reply_text(f"<b>⌯ مرحباً عزيزي :</b> {rank}\n<b> ما هي الاغنيه التي تريد تشغيلها أو البحث عنها</b>")
 
         
 
@@ -39,13 +39,13 @@ async def IdMusic(client: Client, message: Message):
 
 @app.on_message(filters.regex(r"^(اسمي)$"))
 async def NameMusic(client: Client, message: Message):
-    await message.reply_text(f"<b>↯ اسمك : ›</b> {message.from_user.mention}")
+    await message.reply_text(f"<b> اسمك : ›</b> {message.from_user.mention}")
 
 
 
 @app.on_message(filters.regex(r"^(يوزري)$"))
 async def UserMusic(client: Client, message: Message):
-    await message.reply_text(f"<b>↯ يوزرك : ›</b> @{message.from_user.username}")
+    await message.reply_text(f"<b> يوزرك : ›</b> @{message.from_user.username}")
 
 
 
@@ -53,13 +53,13 @@ async def UserMusic(client: Client, message: Message):
 async def BioMusic(client: Client, message: Message):
     usr = await client.get_chat(message.from_user.id)
     bio = usr.bio
-    await message.reply_text(f"""<b>↯ البايو : ›</b> {bio}""")
+    await message.reply_text(f"""<b> البايو : ›</b> {bio}""")
     
 
 
 
 @app.on_message(filters.regex(r"^(بوت الحذف|رابط الحذف)$"))
 async def DeletMusic(client: Client, message: Message):
-    await message.reply_text(f"""<b>↯ بوت الحذف : ›</b> ( @DTeLebot )\n\n<b>↯ رابط الحذف : ›</b> ( <a href="https://my.telegram.org/auth?to=delete">اضغط هنا</a> )""")
+    await message.reply_text(f"""<b> بوت الحذف : ›</b> ( @DTeLebot )\n\n<b> رابط الحذف : ›</b> ( <a href="https://my.telegram.org/auth?to=delete">اضغط هنا</a> )""")
 
 
