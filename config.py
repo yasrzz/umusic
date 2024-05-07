7dd6344a3366247e7ef62c0b513429d95d0ae0b7
bblks <168915058+bblks@users.noreply.github.com>
2024-05-07 19:30:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bblks/AMusic",
+    "https://github.com/bblks/uimusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
