33c660818172d51e455167a3f8e1f05669e0c186
bblks <168915058+bblks@users.noreply.github.com>
2024-06-28 01:01:35 +0300

    Update دعوه.py

--- ZeMusic/plugins/play/دعوه.py
+++ ZeMusic/plugins/play/دعوه.py
@@ -32,3 +32,38 @@ async def brah3(app :app, message:Message):
              await message.reply(f"{text}")
            except:
              pass
+@app.on_message(filters.video_chat_ended)
+async def brah2(client, message):
+    da = message.video_chat_ended.duration
+    ma = divmod(da, 60)
+    ho = divmod(ma[0], 60)
+    day = divmod(ho[0], 24)
+    if da < 60:
+       await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها {da} ثواني وصكرها</b> ")        
+    elif 60 < da < 3600:
+        if 1 <= ma[0] < 2:
+            await message.reply(f" <b>تم انهاء مكالمة الفيديو مدتها دقيقه</b>")
+        elif 2 <= ma[0] < 3:
+            await message.reply(f" <b>تم انهاء مكالمة الفيديو مدتها دقيقتين</b> ")
+        elif 3 <= ma[0] < 11:
+            await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها {ma[0]} دقايق</b> ")  
+        else:
+            await message.reply(f"<b>تم إنهاء مكالمة الفيديو مدتها {ma[0]} دقيقه</b>")
+    elif 3600 < da < 86400:
+        if 1 <= ho[0] < 2:
+            await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها ساعه</b> ")
+        elif 2 <= ho[0] < 3:
+            await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها ساعتين</b> ")
+        elif 3 <= ho[0] < 11:
+            await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها {ho[0]} ساعات</b> ")  
+        else:
+            await message.reply(f"<b>تم إنهاء مكالمة الفيديو مدتها {ho[0]} ساعة</b> ")
+    else:
+        if 1 <= day[0] < 2:
+            await message.reply(f"<b>تم انهاء مكالمة الفيديو مدتها يوم</b> ")
+        elif 2 <= day[0] < 3:
+            await message.reply(f" <b>تم انهاء مكالمة الفيديو مدتها يومين</b> ")
+        elif 3 <= day[0] < 11:
+            await message.reply(f" <b>تم انهاء مكالمة الفيديو مدتها {day[0]} ايام</b> ")  
+        else:
+            await message.reply(f" <b>تم إنهاء مكالمة الفيديو مدتها {day
